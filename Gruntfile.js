88bff2085f654c8c268aa728869777634508f0c6
XfLoops <xfl.zero@gmail.com>
2015-12-28 23:20:28 +0800

    add jsbeautifier

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,8 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
     grunt.initConfig({
         jshint: {
-            files: ['Gruntfile.js', 'plugin/*.js'],
+            files: ['Gruntfile.js', 'clipper.js', 'myscript.js'],
             options: {
                 globals: {
                     jQuery: true
@@ -12,12 +12,38 @@ module.exports = function(grunt) {
         watch: {
             files: ['<%= jshint.files %>'],
             tasks: ['jshint']
+        },
+        jsbeautifier: {
+            files: ['<%= jshint.files %>'],
+            options: {
+                js: {
+                    braceStyle: "collapse",
+                    breakChainedMethods: true,
+                    e4x: false,
+                    evalCode: false,
+                    indentChar: " ",
+                    indentLevel: 0,
+                    indentSize: 4,
+                    indentWithTabs: false,
+                    jslintHappy: true,
+                    keepArrayIndentation: false,
+                    keepFunctionIndentation: false,
+                    maxPreserveNewlines: 10,
+                    preserveNewlines: true,
+                    spaceBeforeConditional: true,
+                    spaceInParen: false,
+                    unescapeStrings: false,
+                    wrapLineLength: 0,
+                    endWithNewline: true
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-jsbeautifier');
 
-    grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('default', ['jshint', 'jsbeautifier']);
 
 };
